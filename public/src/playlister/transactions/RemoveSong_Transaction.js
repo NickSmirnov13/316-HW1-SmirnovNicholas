d1f2ec9508a8f09fc27ce43a10b7cf1f5fc6e0bf
Nicholas Smirnov <nicholassmirnov@Nicholass-MacBook-Air.local>
2025-09-01 18:52:24 -0400

    updated little tweaks

--- public/src/playlister/transactions/RemoveSong_Transaction.js
+++ public/src/playlister/transactions/RemoveSong_Transaction.js
@@ -1,39 +1,32 @@
-import { jsTPS_Transaction } from '../../jstps/index.js'
+import { jsTPS_Transaction } from '../../jstps/index.js';
+import PlaylistSongPrototype from '../PlaylistSongPrototype.js';
 
-/**
- * DeleteSong_Transaction
- * 
- * This class represents a transaction that deletes a song
- * in the playlist. It will be managed by the transaction stack.
- * 
- * @author McKilla Gorilla
- */
 export default class RemoveSong_Transaction extends jsTPS_Transaction {
-    /**
-     * Initializes this object such that it can both do and undo the transaction
-     * 
-     * @param {PlaylisterModel} initModel The M in MVC for this app
-     * @param {number} initIndex The index of where the song is to be created in the playlist
-     * @param {PlaylistSongPrototype} initSong The created song.
-     */
-    constructor(initModel, initIndex, initSong) {
-        super();
-        this.model = initModel;
-        this.index = initIndex;
-        this.song = initSong.clone();
-    }
+  constructor(model, index, initSong) {
+    super();
+    this.model = model;
+    this.index = Number.isInteger(index) ? index : -1;
 
-    /**
-     * Executed when this transaction is first done or redone.
-     */
-    doTransaction() {
-        this.model.removeSong(this.index);
+    // Normalize the song we’ll need for undo
+    if (initSong && typeof initSong.clone === 'function') {
+      this.song = initSong.clone();
+    } else if (initSong) {
+      this.song = new PlaylistSongPrototype(
+        initSong.title ?? "Untitled",
+        initSong.artist ?? "???",
+        initSong.youTubeId ?? "dQw4w9WgXcQ",
+        initSong.year ?? null
+      );
+    } else {
+      this.song = null;
     }
+  }
 
-    /**
-     * Executed when this transaction is undone.
-     */
-    undoTransaction() {
-        this.model.createSong(this.index, this.song);
-    }
+  doTransaction() {
+    if (this.index >= 0) this.model.removeSong(this.index);
+  }
+
+  undoTransaction() {
+    if (this.index >= 0 && this.song) this.model.createSong(this.index, this.song);
+  }
 }
